7cf4f8d095193f28119aa9809f5e2321b9744f4c
Diniboy1123 <Diniboy1123@users.noreply.github.com>
2023-06-17 13:00:39 +0200

    hack: use all cores in build script

--- build-webos.sh
+++ build-webos.sh
@@ -14,7 +14,7 @@ if [[ "$1" == "deps" ]]; then
     git clean -xfd .
     ./bootstrap
     ./configure --with-tarballs=/opt/xbmc-deps/tarballs --host=arm-webos-linux-gnueabi --prefix=/opt/xbmc-deps --with-toolchain=$SDK_PATH --disable-debug
-    make -j16
+    make -j$(nproc --all)
     cd ../../
     #./configure --with-toolchain=$SDK_PATH --prefix=/opt/xbmc-deps --host=arm-webos-linux-gnueabi --with-rendersystem=gles --enable-debug=no --with-target-cflags='-march=armv7-a -mfloat-abi=softfp -mfpu=neon' --with-platform=webos --with-target-cxxflags='-static-libstdc++'
     # make -j16 || make -j16
@@ -24,7 +24,7 @@ fi
 
 make -C tools/depends/target/cmakebuildsys CMAKE_EXTRA_ARGUMENTS="-DSWIG_EXECUTABLE=/opt/xbmc-deps/x86_64-linux-gnu-native/bin/swig"
 cd build
-make -j 16
+make -j$(nproc --all)
 make ipk
 # extra deps: m4 default-jre flex bison
 # make -C native/flatbuffers
